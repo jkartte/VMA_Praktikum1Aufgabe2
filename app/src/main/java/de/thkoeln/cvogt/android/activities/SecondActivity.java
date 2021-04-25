10d23e46acc9042b0b3c0dd8925ba11dc063440d
unknown <julian_alexander.kartte@smail.th-koeln.de>
2021-04-25 13:33:33 +0200

    Aufgabe 2 fertiggestellt

--- app/src/main/java/de/thkoeln/cvogt/android/activities/SecondActivity.java
+++ app/src/main/java/de/thkoeln/cvogt/android/activities/SecondActivity.java
@@ -1,30 +1,36 @@
 package de.thkoeln.cvogt.android.activities;
 
+//LogCat beim Drücken des Back-Buttons in Activity2:
+//04-25 11:07:52.077 13219-13219/de.thkoeln.cvogt.android.activities V/DEMO:       ---> Activity2: onPause() <---
+//04-25 11:07:52.081 13219-13219/de.thkoeln.cvogt.android.activities V/DEMO: ---> Activity1: onStart() <---
+//04-25 11:07:52.081 13219-13219/de.thkoeln.cvogt.android.activities V/DEMO: ---> Activity1: onResume() <---
+//04-25 11:07:52.423 13219-13219/de.thkoeln.cvogt.android.activities V/DEMO:       ---> Activity2: onStop() <---
+//04-25 11:07:52.423 13219-13219/de.thkoeln.cvogt.android.activities V/DEMO:       ---> Activity2: onDestroy() <---
+//Der LogCat zeigt keinen Eintrag für die Methode onSaveInstanceState(). Die Methode müsste eigentlich nach dem
+//Pausieren von Activity 2 ausgeführt werden und an dieser Stelle im LogCat vermerkt werden, anscheinend ist
+//das hier aber nicht passiert.
+
+
 // Programmierung verteilter und mobiler Anwendungen
 // Prof. Dr. Carsten Vogt
 // Technische Hochschule Köln
 // Fakultät für Informations-, Medien- und Elektrotechnik
 // Stand: 4.12.2015
 
-// Eine App mit zwei Activities, zwischen denen man hin- und herschalten kann.
-// LogCat-Ausgaben zeigen dabei an, wann welche Zustandsübergangsmethoden (Callbacks) ausgeführt werden.
-// Video dazu: https://youtu.be/Zwswz3UmvOc
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-/* Diese zweite Aktivität muss ggf. per Hand im Manifest eingetragen werden.
-   Sie wird dort automatisch eingetragen, wenn im Project-Unterfenster auf 'java'
-   rechts-klickt und dann den Menus "New > Activity > ..." folgt. */
 
 public class SecondActivity extends Activity {
 
@@ -32,18 +38,34 @@ public class SecondActivity extends Activity {
     private EditText editText;
 
     public void onCreate(Bundle savedInstanceState) {
-        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        if (savedInstanceState!=null)
+            text=savedInstanceState.getString("StringKey");
+
         editText = (EditText) findViewById(R.id.plain_text_input);
-        String text = editText.getText().toString();
+
+        //Überprüfen, ob die Eingabe in der Variable text gespeichert wurde:
+        final TextView textView = (TextView) findViewById(R.id.displayEditText);
+
+        //TextWatcher ermöglicht das Ausführen von Methoden, bevor/während/nachdem
+        //das editText-Feld geändert wurde.
+        editText.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){};
+            @Override
+            public void afterTextChanged(Editable s) {};
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                text = editText.getText().toString(); //sobald das EditText-Feld geändert wird, wird die Eingabe gespeichert
+                textView.setText(text);
+            };
+        });
 
         Toast.makeText(this,"Activity 2: onCreate()", Toast.LENGTH_LONG).show();
         
         Log.v("DEMO","      ---> Activity2: onCreate() <--- ");
-        // Log.v("DEMO","Activity2: Intent.getComponent() = "+getIntent().getComponent());
-
     }
 
     public void onStart() {
@@ -75,7 +97,7 @@ public class SecondActivity extends Activity {
     public void onSaveInstanceState(Bundle savedInstanceState){
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putString("StringKey",text);
-        Log.v("PRAKTIKUM","---> onSaveInstanceState() <--");
+        Log.v("DEMO","      ---> Activity2: onSaveInstanceState() <--");
     }
 
 }
